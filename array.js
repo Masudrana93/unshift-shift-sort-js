f1161adc235ef0f5af3927f069e176f1a2a2e570
Masud Rana <mr007masudrana@gmail.com>
2025-04-05 16:46:20 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,36 @@
+// Unshift means add data at the starting/before existing data of array
+
+let numbers = [2, 4, 1, 5, 8, 9]; // number data 
+numbers.unshift(27);
+numbers.unshift(15);
+numbers.unshift(12);
+
+console.log(numbers);
+
+let names = ['mamun', 'masud', 'majed']; // string data 
+
+names.unshift('nazir');
+names.unshift('amir');
+
+console.log(names);
+
+
+// Shift means Delete data from the starting/existing data of array
+
+let number = [12, 45, 78, 23, 14];
+number.shift();
+number.shift();
+
+console.log(number);
+
+let name = ['kamal', 'jamal', 'jalal', 'nasir'];
+
+name.shift();
+
+console.log(name);
+
+// Sort means organise data from small to large/letter wise in array
+
+let familyName = ['aman', 'clinton', "borhan", 'delowar', '']
+
+abul
