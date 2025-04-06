42ff7602c37d145022c70fa62a07545018f7d9cd
Masud Rana <mr007masudrana@gmail.com>
2025-04-06 15:31:53 +0600

    object

---
+++ objects.js
@@ -0,0 +1,27 @@
+
+var ranaColor = "black";
+var ranaAge = 35;
+var ranaHeight = 5.5;
+var ranaFriends = ['alom', 'fysal', 'anower'];
+var ranaLocation = "Dhaka"
+
+
+console.log(ranaAge);
+
+
+// using object{}
+
+var rana = {color: "black", age: 35, height: 5.5, friends: ['alom', 'fysal', 'anower'], location: 'Dhaka'}
+
+console.log(rana);
+
+
+var ranaM = {
+    color: "black",
+    age: 35,
+    height: 5.5, 
+    friends: ['alom', 'fysal', 'anower'], 
+    location: 'Dhaka'
+}
+
+console.log(ranaM.friends[1]);
