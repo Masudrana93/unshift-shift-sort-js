75ba0e7544848fbd951fcf5c5e637074c01e7db8
Masud Rana <mr007masudrana@gmail.com>
2025-04-06 16:03:14 +0600

    add, change, delete data in object from outside

--- objects.js
+++ objects.js
@@ -25,3 +25,48 @@ var ranaM = {
 }
 
 console.log(ranaM.friends[1]);
+
+
+// add data in object from outside 
+
+var myObj = {
+    name : "alom",
+    age: 34,
+    email: 'alom12@gmail.com',
+    location: "cumilla",
+    height: 5.6,
+}
+myObj.color = "black"
+myObj.birthYear = 1988
+
+console.log(myObj);
+
+
+// change data in object from outside 
+
+var myObjTwo = {
+    name : "aklom",
+    age: 43,
+    email: 'aklom45@gmail.com',
+    location: "Sylhet",
+    height: 5.4,
+}
+myObjTwo.location = "rangpur"
+myObjTwo.birthYear = 1981
+
+console.log(myObjTwo);
+
+
+// delete data in object from outside 
+
+var myObjOne = {
+    name : "aklom",
+    age: 43,
+    email: 'aklom45@gmail.com',
+    location: "Sylhet",
+    height: 5.4,
+}
+delete myObjOne.location;
+
+
+console.log(myObjOne);
